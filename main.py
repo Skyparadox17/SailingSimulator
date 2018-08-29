cde41e70e7b6cbdb189c40066e1e7f2afa6900b9
Skyparadox17 <ella@kirosoft.co.uk>
2018-08-29 22:41:04 +0100

    Tidy up. Unused code deleted. Understanding maths/physics behind previous code.

--- main.py
+++ main.py
@@ -8,9 +8,6 @@ from panda3d.core import LPoint3, LVector3, NodePath
 from direct.gui.OnscreenText import OnscreenText
 from direct.task.Task import Task
 from math import sin, cos, pi
-from random import randint, choice, random
-from direct.interval.MetaInterval import Sequence
-from direct.interval.FunctionInterval import Wait, Func
 import sys
 
 # Constants that will control the behavior of the game. It is good to
@@ -25,16 +22,6 @@ ACCELERATION = 10   # Ship acceleration in units/sec/sec
 MAX_VEL = 6         # Maximum ship velocity in units/sec
 MAX_VEL_SQ = MAX_VEL ** 2  # Square of the ship velocity
 DEG_TO_RAD = pi / 180  # translates degrees to radians for sin and cos
-BULLET_LIFE = 2     # How long bullets stay on screen before removed
-BULLET_REPEAT = .2  # How often bullets can be fired
-BULLET_SPEED = 10   # Speed bullets move
-AST_INIT_VEL = 1    # Velocity of the largest asteroids
-AST_INIT_SCALE = 3  # Initial asteroid scale
-AST_VEL_SCALE = 2.2  # How much asteroid speed multiplies when broken up
-AST_SIZE_SCALE = .6  # How much asteroid scale changes when broken up
-AST_MIN_SCALE = 1.1  # If and asteroid is smaller than this and is hit,
-# it disapears instead of splitting up
-
 
 # This helps reduce the amount of code used by loading objects, since all of
 # the objects are pretty much the same.
@@ -90,11 +77,10 @@ class SailingSimulator(ShowBase):
         self.sailHolder = NodePath("sailHolder")
         self.sailHolder.reparentTo(self.ship)
         self.sailHolder.setPos(0, 0, 0.2)
-        self.sailHolder.setR(25)
+        #self.sailHolder.setR(25)
 
         # Load the sail into holder
         self.sail = loadObject("sail2.png", scale=1, depth=50)
-        #self.setVelocity(self.sail, LVector3.zero())
         self.sail.reparentTo(self.sailHolder)
         self.sail.setPos(0, 0, -0.3)
 
@@ -105,7 +91,7 @@ class SailingSimulator(ShowBase):
         self.rudder = loadObject("sail2.png", scale=.4)
         self.rudder.reparentTo(self.rudderHolder)
         self.rudder.setPos(0, 0, -0.1)
-        self.rudderHolder.setR(-25)
+        #self.rudderHolder.setR(-25)
 
         # A dictionary of what keys are currently being pressed
         # The key events update this list, and our task will query it as input
@@ -141,13 +127,7 @@ class SailingSimulator(ShowBase):
     def getVelocity(self, obj):
         return obj.getPythonTag("velocity")
 
-    def setExpires(self, obj, val):
-        obj.setPythonTag("expires", val)
-
-    def getExpires(self, obj):
-        return obj.getPythonTag("expires")
-
-    # This is our main task function, which does all of the per-frame
+   # This is our main task function, which does all of the per-frame
     # processing.  It takes in self like all functions in a class, and task,
     # the task object returned by taskMgr.
     def gameLoop(self, task):
@@ -172,6 +152,7 @@ class SailingSimulator(ShowBase):
     # Updates the positions of objects
     def update_pos(self, obj, dt):
         vel = self.getVelocity(obj)
+        # newpos - SOH this does the maths bit for me
         newPos = obj.getPos() + (vel * dt)
 
         # Check if the object is out of bounds. If so, wrap it
@@ -207,8 +188,7 @@ class SailingSimulator(ShowBase):
             # relative to the camera, the screen in Panda is the XZ plane.
             # Therefore all of our Y values in our velocities are 0 to signify
             # no change in that direction.
-            newVel = \
-                LVector3(sin(heading_rad), 0, cos(heading_rad)) * ACCELERATION * dt
+            newVel = LVector3(sin(heading_rad), 0, cos(heading_rad)) * ACCELERATION * dt
             newVel += self.getVelocity(self.ship)
             # Clamps the new velocity to the maximum speed. lengthSquared() is
             # used again since it is faster than length()
